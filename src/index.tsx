10da1209724ee40d4d9ee13a80b365e94c0771d5
WebDev-dz <webdevdz2@gmail.com>
2025-01-04 23:37:20 +0100

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -5,13 +5,7 @@ import './index.css';
 import App from './App';
 import { ClerkProvider } from '@clerk/clerk-react'
 
-const PUBLISHABLE_KEY = process.env.REACT_APP_CLERK_PUBLISHABLE_KEY
 
-
-console.log({env : process.env})
-if (!PUBLISHABLE_KEY) {
-  throw new Error('Add your Clerk Publishable Key to the .env.local file')
-}
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
